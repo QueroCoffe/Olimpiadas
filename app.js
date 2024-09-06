c8c1ae05a6c86106efdd191d7730bee0e5027674
William dos Santos Rodrigues <120606300+QueroCoffe@users.noreply.github.com>
2024-09-06 19:14:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function pesquisar() {
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  //condição para não mostrar os dados quando a barra de pesquisa tiver em branco
+  if (campoPesquisa == "") {
+    section.innerHTML = `
+        <div class="item-resultado">
+            <h2>Digite algo!</h2>
+        </div>
+    `;
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      // Cria um novo elemento HTML para cada resultado
+      resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+            `;
+    }
+  }
+
+  if (!resultados) {
+    resultados = `
+    <div class="item-resultado">
+      <h2>Desculpe, Nenhum dado encontrado!</h2>
+    <div>
+    `;
+  }
+
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
+}
